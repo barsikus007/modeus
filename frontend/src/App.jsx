f11bb9042f868ccacfbf11d1af282ff338d11938
aartaka <aartaka@protonmail.com>
2022-03-13 13:02:24 +0500

    frontend/src/App: Properly resolve professorHome reference.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import ProfessorHome from 'components/ProfessorHome';
+import ProfessorHome from './components/ProfessorHome';
 
 function App() {
   return (
